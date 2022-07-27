55330c9e6286be047599d38cb92faab58e3a4df5
Son Hong Su <son.ho463@gmail.com>
2022-07-27 20:06:58 +0900

    ハンバーガー基本の挙動

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,21 @@
 const faqItems = document.querySelectorAll(".js-faq-item");
 const faqQuestions = document.querySelectorAll(".p-faq__question");
 
+faqQuestions.forEach((q) => {
+  q.addEventListener("click", function () {
+    const next = q.nextElementSibling;
+    if (next.classList.contains("open")) {
+      next.classList.remove("open");
+    } else {
+      next.classList.add("open");
+    }
+  });
+});
 
-faqQuestions.forEach((q)=>{
-	q.addEventListener('click',function(){
-		const next = q.nextElementSibling;
-		if(next.classList.contains('open')){
-			next.classList.remove('open')
-		} else {
-			next.classList.add('open')
-		}
-	})
-})
+const header = document.querySelector('.l-header')
+
+const navOpen = () => {
+	header.classList.contains('nav-open')
+	? header.classList.remove('nav-open')
+	: header.classList.add('nav-open')
+};
